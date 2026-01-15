72c5a13c6d8998de598fc14e3f603c95729fe1e4
Purushottam Kumar <pstechb@gmail.com>
2026-01-15 21:02:47 +0530

    Render deploy fix

--- app.py
+++ app.py
@@ -87,6 +87,10 @@ def backup():
     shutil.copy(DB, name)
     return send_file(name, as_attachment=True)
 
+import os
+
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
